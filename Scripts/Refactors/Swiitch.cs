3847b4ad4a382ccbf3e13588a718de55f770606d
Vincent Lennuel Llanto <72075830+soulevil098123@users.noreply.github.com>
2023-05-28 00:54:26 +0800

    initial commit

---
+++ Scripts/Refactors/Swiitch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Swiitch : MonoBehaviour
+{
+   
+    public GameObject[] background;
+    public int pages;
+
+    [SerializeField]
+    TextSpeechController speechController;
+
+    [SerializeField]
+    public int maxPage = 11;
+
+    void Start()
+    {
+        pages = 0;
+        background[0].gameObject.SetActive(true);
+    }
+        
+
+    private void _UpdatePage(int page){
+      background[pages].gameObject.SetActive(false);
+      pages = page;
+      background[pages].gameObject.SetActive(true);
+      if(speechController.autoSpeak)
+         speechController.OnStartSpeak();
+    }
+
+
+    public void Next()
+     {
+         if(pages + 1 > maxPage) return;
+         _UpdatePage(pages + 1);
+     }
+    
+     public void Previous()
+     {
+         if(pages - 1 < 0) return;
+         _UpdatePage(pages - 1);
+     }
+
+   
+}
---
+++ Scripts/TextToSpeech/TextSpeechController.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TextSpeech;
+using UnityEngine.Android;
+using System.Text.RegularExpressions;
+using TMPro;
+
+public class TextSpeechController : MonoBehaviour
+{
+    
+    [SerializeField]
+    public string tag; //Gameobject tag, used for getting text mesh pro text that will be used for text to speech
+    [SerializeField]
+    public string lang = "en-US";  
+    [Range(0.5f, 2)]
+    public float pitch = 1f;
+    [Range(0.5f, 2)]
+    public float rate = 1f;
+
+    public bool autoSpeak  = false;
+    private bool _isSpeaking = false;
+
+
+    void Start()
+    {
+        _isSpeaking = false;
+        _Setting(lang, pitch, rate);
+
+        //Callback whenever the speech has started or ended
+        TextToSpeech.Instance.onStartCallBack = _OnStart;
+        TextToSpeech.Instance.onDoneCallback = _OnEnd;
+    }
+
+    //Set isSpeaking to true if started
+    private void _OnStart(){
+        _isSpeaking = true;
+    }
+
+    //Set isSpeaking to false if ended
+    private void _OnEnd(){
+        _isSpeaking = false;
+    }
+
+    //Method for speak
+    public void OnStartSpeak()
+    {
+
+        if(autoSpeak){
+            OnStopSpeak();
+        }
+
+        if(_isSpeaking) return;
+        
+        string message = "";
+
+        //Will search for all gameobject with the selected tag
+        foreach (GameObject item in GameObject.FindGameObjectsWithTag(tag))
+            //Gameobject should be active
+            if(item.activeSelf){
+                TMP_Text component = item.GetComponent<TMP_Text>();
+                if(!!item) message+=".\n "+component.text;
+            }
+
+        string newMessage = Regex.Replace(message, @"<[^>]+>|&nbsp;|&zwnj;|&raquo;|&laquo;", "").Trim();
+
+        TextToSpeech.Instance.StartSpeak(newMessage);
+    }
+
+    //Switching auto speak
+    public void SwitchAuto(){
+        autoSpeak = !autoSpeak;
+        
+        if(autoSpeak) {
+            OnStartSpeak();
+        }else{
+            OnStopSpeak();
+        }
+
+    }
+
+    //Method to stop speak
+    public void OnStopSpeak()
+    {
+        _isSpeaking = false;
+        TextToSpeech.Instance.StopSpeak();
+    }
+
+    //Settings
+    private void _Setting(string code, float pitch, float rate)
+    {
+        TextToSpeech.Instance.Setting(code, 1, 1);
+    }
+
+
+}
